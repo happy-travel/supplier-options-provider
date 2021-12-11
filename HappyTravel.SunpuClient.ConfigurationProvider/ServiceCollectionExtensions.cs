2fad24690257e571649cfbaf601a96d7beb8ddea
Denis Nikolaev <denis.nikolaev@happytravel.com>
2021-12-11 07:05:23 +0500

    inital

---
+++ HappyTravel.SunpuClient.ConfigurationProvider/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace HappyTravel.SunpuClient.ConfigurationProvider
+{
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection ConfigureSuppliers(this IServiceCollection services, IConfiguration configuration)
+        {
+            return services.Configure<SupplierOptions>(configuration.GetSection(SunpuConfigurationProvider.SectionName));
+        }
+    }
+}
