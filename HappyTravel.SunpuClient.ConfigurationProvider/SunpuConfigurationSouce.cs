35a1ae601cbf6c5f7f9e13a02f01e3bd6cc4efec
Konstantin Yefremov <konstyefremov@gmail.com>
2022-01-18 06:30:55 +0500

    Automatically update suppliers and authorize requests

--- HappyTravel.SunpuClient.ConfigurationProvider/SunpuConfigurationSouce.cs
+++ HappyTravel.SunpuClient.ConfigurationProvider/SunpuConfigurationSouce.cs
@@ -4,16 +4,28 @@ namespace HappyTravel.SunpuClient.ConfigurationProvider
 {
     public class SunpuConfigurationSource : IConfigurationSource
     {
-        public SunpuConfigurationSource(string endpoint)
+        public SunpuConfigurationSource(string endpoint, 
+            string identityUrl, 
+            string clientId, 
+            string clientSecret, 
+            string clientScope)
         {
             _endpoint = endpoint;
+            _identityUrl = identityUrl;
+            _clientId = clientId;
+            _clientSecret = clientSecret;
+            _clientScope = clientScope;
         }
     
     
         public IConfigurationProvider Build(IConfigurationBuilder builder) 
-            => new SunpuConfigurationProvider(_endpoint);
+            => new SunpuConfigurationProvider(_endpoint, _identityUrl, _clientId, _clientSecret, _clientScope);
 
 
         private readonly string _endpoint;
+        private readonly string _identityUrl;
+        private readonly string _clientId;
+        private readonly string _clientSecret;
+        private readonly string _clientScope;
     }
 }
