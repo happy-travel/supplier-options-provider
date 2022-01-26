eac56a62905452a0a06ce526974bef4bcc141970
Konstantin Yefremov <konstyefremov@gmail.com>
2022-01-26 06:18:08 +0500

    Remove integration console

--- HappyTravel.SunpuClient.IntegrationConsole/Program.cs
+++
@@ -1,9 +0,0 @@
-using HappyTravel.SunpuClient.ConfigurationProvider;
-
-var builder = WebApplication.CreateBuilder(args);
-
-builder.Services.AddSunpuClient(o =>
-{
-    o.EndPoint = "http://localhost:5990/api/1.0/suppliers";
-    o.HttpClientName = "Edo";
-});
