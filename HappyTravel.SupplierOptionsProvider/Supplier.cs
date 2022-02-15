a0da1c79b743cdcb8df0b4635d53f95b2abb8eb3
Denis Nikolaev <denis.nikolaev@happytravel.com>
2022-02-15 08:46:45 +0500

    added custom headers

--- HappyTravel.SupplierOptionsProvider/Supplier.cs
+++ HappyTravel.SupplierOptionsProvider/Supplier.cs
@@ -1,4 +1,6 @@
-﻿namespace HappyTravel.SupplierOptionsProvider
+﻿using System.Collections.Generic;
+
+namespace HappyTravel.SupplierOptionsProvider
 {
     public class SlimSupplier
     {
@@ -9,5 +11,6 @@
         public string ConnectorUrl { get; init; } = string.Empty;
         public string? ConnectorGrpcEndpoint { get; set; }
         public bool IsMultiRoomFlowSupported { get; init; }
+        public Dictionary<string, string>? CustomHeaders { get; init; }
     }
 }
