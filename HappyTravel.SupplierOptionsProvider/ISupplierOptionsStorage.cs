ba87aa3f13ad8e3b959e268100fe9b38d6f23c69
vadim <vadim.mingazhev@gmail.com>
2022-02-20 10:45:23 +0500

    Added get by code method

--- HappyTravel.SupplierOptionsProvider/ISupplierOptionsStorage.cs
+++ HappyTravel.SupplierOptionsProvider/ISupplierOptionsStorage.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using HappyTravel.SupplierOptionsClient.Models;
 
@@ -6,7 +7,9 @@ namespace HappyTravel.SupplierOptionsProvider
     public interface ISupplierOptionsStorage
     {
         List<SlimSupplier> GetAll();
+        [Obsolete("Supplier id usage must be replaced by supplier code usage")]
         SlimSupplier GetById(int id);
         void Set(List<SlimSupplier> suppliers);
+        SlimSupplier GetByCode(string code);
     }
 }
