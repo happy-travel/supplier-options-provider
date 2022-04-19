218ae1de7070ff117a2e89d26cb93972804045c7
oleg-n-k <konovalov.onk@gmail.com>
2022-04-20 00:53:54 +0300

    Add supplier priorities

--- HappyTravel.SupplierOptionsProvider/SupplierOptionsStorage.cs
+++ HappyTravel.SupplierOptionsProvider/SupplierOptionsStorage.cs
@@ -16,31 +16,46 @@ namespace HappyTravel.SupplierOptionsProvider
         
         
         public Result<List<SlimSupplier>> GetAll() 
-            => SpinWait.SpinUntil(() => _isFilled, _supplierOptionsProviderConfiguration.StorageTimeout) 
+            => SpinWait.SpinUntil(() => _suppliersAreFilled, _supplierOptionsProviderConfiguration.StorageTimeout) 
                 ? _suppliers 
                 : Result.Failure<List<SlimSupplier>>("Supplier storage is not filled");
 
 
         public Result<SlimSupplier> Get(string code)
         {
-            if (!SpinWait.SpinUntil(() => _isFilled, _supplierOptionsProviderConfiguration.StorageTimeout))
+            if (!SpinWait.SpinUntil(() => _suppliersAreFilled, _supplierOptionsProviderConfiguration.StorageTimeout))
                 return Result.Failure<SlimSupplier>("Supplier storage is not filled");
 
             var supplier = _suppliers.SingleOrDefault(s => s.Code == code);
             return supplier ?? Result.Failure<SlimSupplier>("Supplier not found");
         }
+
         
+        public Result<SupplierPriorityByTypes> GetPriorities() 
+            => SpinWait.SpinUntil(() => _supplierPrioritiesAreFilled, _supplierOptionsProviderConfiguration.StorageTimeout)
+                ? _supplierPriorities
+                : Result.Failure<SupplierPriorityByTypes>("Supplier storage is not filled");
+
         
         public void Set(List<SlimSupplier> suppliers)
         {
             _suppliers = suppliers;
-            _isFilled = true;
+            _suppliersAreFilled = true;
         }
 
 
+        public void Set(SupplierPriorityByTypes priorities)
+        {
+            _supplierPriorities = priorities;
+            _supplierPrioritiesAreFilled = true;
+        }
+
+        
         private readonly SupplierOptionsProviderConfiguration _supplierOptionsProviderConfiguration;
         
         private volatile List<SlimSupplier> _suppliers = new();
-        private bool _isFilled;
+        private volatile SupplierPriorityByTypes _supplierPriorities = new();
+        private bool _suppliersAreFilled;
+        private bool _supplierPrioritiesAreFilled;
     }
 }
--- HappyTravel.SupplierOptionsProvider/SupplierOptionsUpdater.cs
+++ HappyTravel.SupplierOptionsProvider/SupplierOptionsUpdater.cs
@@ -46,13 +46,31 @@ namespace HappyTravel.SupplierOptionsProvider
 
         private async Task UpdateStorage(ISupplierOptionsClient client, ILogger logger)
         {
-            var (_, isFailure, suppliers, error) = await client.GetAll();
-            if (isFailure)
-                throw new Exception($"Supplier options storage update failed: {error}");
+            var getSuppliersTask = client.GetAll();
+            var getPrioritiesTask = client.GetPriorities();
+
+            await Task.WhenAll(getSuppliersTask, getPrioritiesTask);
+            
+            var (_, isGettingSuppliersFailure, suppliers, suppliersGettingError) = await getSuppliersTask;
+            var (_, isGettingPrioritiesFailure, priorities, prioritiesGettingError) = await getPrioritiesTask;
+            
+            if (isGettingSuppliersFailure || isGettingPrioritiesFailure)
+            {
+                var error = "Supplier options storage update failed: ";
+                if (isGettingSuppliersFailure)
+                    error += suppliersGettingError;
+                else if (isGettingPrioritiesFailure)
+                    error += prioritiesGettingError;
+                        
+                throw new Exception(error);
+            }
             
             _storage.Set(suppliers);
+            _storage.Set(priorities);
+            
             logger.LogSuppliersStorageRefreshed(suppliers.Count);
         }
+        
 
         private readonly IServiceScopeFactory _scopeFactory;
         private readonly ISupplierOptionsStorage _storage;
